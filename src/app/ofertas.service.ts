fb873eac08c54d78261bf32bf7881a9c2baa3fb8
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-10-29 15:17:06 -0300

    inclusão de observable e convertendo-o para uma promise e exclusão do array

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,94 +1,22 @@
-import{ Oferta} from './shared/oferta.model'
-import { promise } from 'protractor'
-import { resolve } from 'q'
+import { HttpClient } from '@angular/common/http'; //importar desse jeito, lembrar de quando for se referir a esse importe ser httpclient
+import { Injectable} from '@angular/core';
+import{ Oferta} from './shared/oferta.model';
 
+
+@Injectable()
 export class OfertasService{
-
-    public ofertas: Oferta[] = 
-        [
-            {
-                id: 1,
-                categoria: "restaurante",
-                titulo: "Super Burger",
-                descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-                anunciante: "Original Burger",
-                valor: 29.90,
-                destaque: true,
-                imagens: [
-                    {url: "/assets/ofertas/1/img1.jpg"},
-                    {url: "/assets/ofertas/1/img2.jpg"},
-                    {url: "/assets/ofertas/1/img3.jpg"},
-                    {url: "/assets/ofertas/1/img4.jpg"}
-                ]
-            },
-            {
-                id: 2,
-                categoria: "restaurante",
-                titulo: "Cozinha Mexicana",
-                descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-                anunciante: "Mexicana",
-                valor: 32.90,
-                destaque: true,
-                imagens: [
-                    {url: "/assets/ofertas/2/img1.jpg"},
-                    {url: "/assets/ofertas/2/img2.jpg"},
-                    {url: "/assets/ofertas/2/img3.jpg"},
-                    {url: "/assets/ofertas/2/img4.jpg"}
-                ]
-            
-            },
-            {
-                id: 3,
-                categoria: "diversao",
-                titulo: "Estância das águas",
-                descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-                anunciante: "Estância das águas",
-                valor: 31.90,
-                destaque: true,
-                imagens: [
-                    {url: "/assets/ofertas/3/img1.jpg"},
-                    {url: "/assets/ofertas/3/img2.jpg"},
-                    {url: "/assets/ofertas/3/img3.jpg"},
-                    {url: "/assets/ofertas/3/img4.jpg"},
-                    {url: "/assets/ofertas/3/img5.jpg"},
-                    {url: "/assets/ofertas/3/img6.jpg"}
-                ]
-            }
-        ]
     
-        
+    constructor(private http: HttpClient){}
     
-public getOfertas(): Array<Oferta>{ 
-    return this.ofertas
-    }
-    public getOfertas2(): Promise<Oferta[]>{
-        return new Promise ((resolve, reject) => {
-            // algum tipo de processamento, que ao finalizar chama a função resolve ou a função reject
-            let deucerto = true
-            if (deucerto) {
+public getOfertas(): Promise<Oferta[]> 
+{
+    
 
-            setTimeout(() => resolve(this.ofertas), 3000)
-            resolve (this.ofertas)}
+    //efetuar uma requisição http
+    return this.http.get('http://localhost:3000/ofertas')//observable
+    .toPromise()  //para converter esse observable para uma promise 
+    .then((resposta: any) => resposta)
+    //retornar uma promise Oferta[]
 
-            else{ 
-            reject({ codigo_erro:404, mensagem_erro: 'Servior não encontrado ' }) // caso for reseitado 
-            }
-        }) //isso é uma função de call back
-        .then(( ofertas: Oferta[]) => {
-            //fazer alguma tratativa
-            
-            return ofertas   
-        })
-        .then(( ofertas: Oferta[]) => {
-            //segundo then
-            
-            return new Promise((resolve2, reject2) => { // isso aqui simula uma lentidão por 3 segundos
-                setTimeout(() =>{ resolve2( ofertas )} ,3000)
-            })   
-        })
-        .then(( ofertas: Oferta[] ) => {
-            //terceiro then executando após 3 segundos porque estava aguardando uma promisse ser resolvida
-            return ofertas
-        })
     }
 }
