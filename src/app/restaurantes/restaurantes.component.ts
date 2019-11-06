1eae0a7186b281a9c9a6177f81e7f0cac35928b7
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-11-06 11:54:08 -0300

    apenas um console.log

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from "../shared/oferta.model";
+import { OfertasService } from "../ofertas.service";
+import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers: [ OfertasService]
 })
 export class RestaurantesComponent implements OnInit {
+  public ofertas: Oferta[]
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertasService.getOfertasPorCategoria('restaurantes')//restorna uma promise
+    .then(( ofertas: Oferta[] ) => { //retorna um array
+      this.ofertas = ofertas
+     
+      console.log(this.ofertas)
+    })
+
   }
 
 }
