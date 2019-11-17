036ef0efed0b0879fef9959d32c1949cd3523931
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-11-17 17:50:44 -0300

    inclusão de nova rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Routes,RouterModule} from '@angular/router';
 import { HomeComponent} from './home/home.component';
 import { RestaurantesComponent} from './restaurantes/restaurantes.component';
 import { DiversaoComponent} from './diversao/diversao.component';
+import{OfertaComponent } from './oferta/oferta.component';
 
 export const ROUTES: Routes =[
     { path: '', component: HomeComponent },
     { path:'restaurantes', component: RestaurantesComponent},
     { path:'diversao', component: DiversaoComponent},
+    { path:'oferta', component: HomeComponent },
+    { path:'oferta/:id', component: OfertaComponent },
 ]
