3b168406772466d0e738fa95e3c040fa15594467
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-11-28 11:24:59 -0300

    ajustando url de acesso a API

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,9 +2,12 @@ import { HttpClient } from '@angular/common/http'; //importar desse jeito, lembr
 import { Injectable} from '@angular/core';
 import{ Oferta} from './shared/oferta.model';
 
+import{ URL_API} from './app.api';
+
 
 @Injectable()
 export class OfertasService{
+    private url_api = 'http://localhost:3000/ofertas' //ponto unico para modificação
     
     constructor(private http: HttpClient){}
     
@@ -12,18 +15,18 @@ export class OfertasService{
 public getOfertas(): Promise<Oferta[]> 
 {
     //efetuar uma requisição http
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')//observable
+    return this.http.get(`${URL_API}?destaque=true`)//observable
     .toPromise()  //para converter esse observable para uma promise 
     .then((resposta: any) => resposta)
     //retornar uma promise Oferta[]
     }
     public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any)=>  resposta)//objeto literal
     }
     public getOfertasPorId(id: number): Promise<Oferta>{//vai retornar uma promise
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)// filtra através dos atributos de cada objeto
+    return this.http.get(`${URL_API}?id=${id}`)// filtra através dos atributos de cada objeto
     .toPromise()//converte uma observable para uma promise
     .then((resposta: any)=> { // quando estiver resolvido recebe uma resposta com base na ação
         //o metodo .ishift ele extrai a primeira posição de um array 
